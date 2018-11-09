cf79e82b1ef4f3dbc096d27a1e68892e89f60e1e
Luke Marrai <Luke.Marrai@softwire.com>
2018-11-09 12:07:07 +0000

    Finished Part 1.

---
+++ BusBoard.ConsoleApp/BusPrediction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+
+namespace BusBoard.ConsoleApp
+{
+    public class BusPrediction : IComparable<BusPrediction>
+    {
+        public BusPrediction()
+        {
+        }
+
+        public string stationName { get; set; }
+        public string lineId { get; set; }
+        public string lineName { get; set; }
+        public string platformName { get; set; }
+        public string direction { get; set; }
+        public string destinationName { get; set; }
+        public DateTime timestamp { get; set; }
+        public int timeToStation { get; set; }
+        public string currentLocation { get; set; }
+        public string towards { get; set; }
+        public DateTime expectedArrival { get; set; }
+
+        public override string ToString()
+        {
+            return lineName + " arriving at " + expectedArrival;
+        }
+
+        public int CompareTo(BusPrediction other)
+        {
+            return this.expectedArrival.CompareTo(other.expectedArrival);
+        }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BusBoard.ConsoleApp
 {
@@ -10,6 +15,27 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        var client = new RestClient();
+        client.BaseUrl = new Uri("https://api.tfl.gov.uk");
+
+        var input = Console.ReadLine();
+
+        var request = new RestRequest();
+        var keys = File.ReadAllLines("C:\\Work\\Training\\BusBoard\\BusBoard\\keys.txt");
+        request.Resource = "StopPoint/" + input +"/Arrivals?app_id=" + keys[0] +"&app_key="+keys[1];
+
+        var response = client.Execute<List<BusPrediction>>(request);
+        var busArrivals = response.Data;
+        busArrivals.Sort();
+
+        for (int i = 0; i < 5 && i<= busArrivals.Count; i++)
+        {
+            Console.WriteLine(busArrivals[i].ToString());
+        }
+        
+
+        Console.ReadLine();
     }
   }
 }
